5441d0d13d5f82b59b0763731cceae7b291fd352
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-06-13 17:53:47 +0700

    Create summary component

--- client/src/components/Summary/Summary.tsx
+++ client/src/components/Summary/Summary.tsx
@@ -1,9 +1,28 @@
 import { FC } from "react";
 import style from "./Summary.module.css";
+import { selectLanguageJson } from "../../features/optionsSlice";
+import { useSelector } from "react-redux";
+import photograph from "../../assets/images/summary/photograph.jpg"
 
 const Summary: FC = () => {
+
+    const language = useSelector(selectLanguageJson);
+    const { title, description} = language.summary;
+
+
     return (
         <div className={style.MainContainer}>
+            <div className={style.LeftContainer}>
+                <h1>
+                    {title}
+                </h1>
+                <h3>
+                    {description}
+                </h3>
+            </div>
+            <div className={style.RightContainer}>
+                <img src={photograph} alt=""/>
+            </div>
         </div>
     )
 }
