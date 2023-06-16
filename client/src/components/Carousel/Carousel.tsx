f421b3100d635bf3e9e7495faf7fe10861951cac
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-06-16 12:06:55 +0700

    Implement carousel

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,27 +1,54 @@
-import { FC } from "react";
-import style from  "./Carousel.module.css";
-import Experience from "../Experience/Experience";
-import Tecnologies from "../Tecnologies/Tecnologies";
-import WhoIAm from "../WhoIAm/WhoIAm";
-import { useState } from "react";
+    import { FC, MouseEventHandler } from "react";
+    import style from  "./Carousel.module.css";
+    import Experience from "../Experience/Experience";
+    import Tecnologies from "../Tecnologies/Tecnologies";
+    import WhoIAm from "../WhoIAm/WhoIAm";
+    import { useState } from "react";
 
-const Carousel: FC = () => {
-    
-    const [activeIndex, setActiveIndex] = useState(0);
-    return (
-        <>
-        <div className={style.Carousel}>
-            <WhoIAm/>
-            <Tecnologies/>
+    const Carousel: FC = () => {
+        
+        const [activeIndex, setActiveIndex] = useState(0);
+
+        const items = [
+            <WhoIAm/>,
+            <Tecnologies/>,
             <Experience/>
-        </div>
-        <div className={style.Background}>
-            <div className={style.Rombos}></div>
-            <div className={style.Rombos}></div>
-            <div className={style.Rombos}></div>
-        </div>
-        </>
-    )
-}
+        ];
 
-export default Carousel;
+        const updateIndex = (newIndex: number) => {
+            if (newIndex < 0) {
+                newIndex = 0
+            } else if (newIndex >= items.length) {
+                newIndex = 0;
+            }
+            setActiveIndex(newIndex);
+        }
+
+
+
+        return (
+            <>
+            <div className={style.Carousel}
+                style={{transform: `translateX(-${activeIndex * 33.2}%)`}}>
+
+                {items.map((element) => {
+                    return element;
+                })}
+
+            </div>
+            <div className={style.Background}>
+                <div className={`${style.Rombos} ${activeIndex === 0 ? style.ActiveIndex : ""}`} onClick={()=> {
+                    updateIndex(0);
+                }}></div>
+                <div className={`${style.Rombos} ${activeIndex === 1 ? style.ActiveIndex : ""}`} onClick={() => {
+                    updateIndex(1);
+                }}></div>
+                <div className={`${style.Rombos} ${activeIndex === 2 ? style.ActiveIndex : ""}`} onClick={()=> {
+                    updateIndex(2);
+                }}></div>
+            </div>
+            </>
+        )
+    }
+
+    export default Carousel;
