93c228bb0b4071b72d9167b520743175055d3df2
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-06-20 17:17:40 +0700

    Finish css preview styling

--- client/src/components/Preview/Preview.tsx
+++ client/src/components/Preview/Preview.tsx
@@ -3,6 +3,13 @@ import style from "./Preview.module.css";
 import musclelab_a from "../../assets/images/details/musclelab/musclelab1.png";
 import musclelab_b from "../../assets/images/details/musclelab/musclelab2.png";
 import musclelab_c from "../../assets/images/details/musclelab/musclelab3.png";
+import portfolio_a from "../../assets/images/details/portfolio/portfolio1.png";
+import portfolio_b from "../../assets/images/details/portfolio/portfolio2.png";
+import portfolio_c from "../../assets/images/details/portfolio/portfolio3.png";
+import portfolio_d from "../../assets/images/details/portfolio/portfolio4.png";
+import doggofinder_a from "../../assets/images/details/doggofinder/dogapi1.png";
+import doggofinder_b from "../../assets/images/details/doggofinder/dogapi2.png";
+import doggofinder_c from "../../assets/images/details/doggofinder/dogapi3.png";
 
 interface PreviewType {
     nameParam: string;
@@ -13,19 +20,11 @@ const Preview: FC<PreviewType> = ({ nameParam }) => {
     const imageSelector = (nameParam: string): any => {
         switch (nameParam) {
             case "portfolio":
-                return [
-                    // portfolio_a,
-                    // portfolio_b,
-                    // portfolio_c
-                ];
+                return [ portfolio_a,portfolio_b, portfolio_c, portfolio_d ];
             case "musclelab":
-                return [musclelab_a, musclelab_b, musclelab_c];
+                return [ musclelab_a, musclelab_b, musclelab_c ];
             case "doggofinder":
-                return [
-                    // doggofinder_a,
-                    // doggofinder_b,
-                    // doggofinder_c
-                ];
+                return [ doggofinder_a, doggofinder_b, doggofinder_c ];
             default:
                 return [musclelab_a, musclelab_b, musclelab_c];
         }
@@ -35,13 +34,22 @@ const Preview: FC<PreviewType> = ({ nameParam }) => {
 
     return (
         <div className={style.Container}>
-            <div className={style.ToolBar}>
-                <div className={style.RedDot}></div>
-                <div className={style.YellowDot}></div>
-                <div className={style.GreenDot}></div>
-                <div className={style.SearchBar}></div>
-            </div>
-                <img src={images[0]} alt="" />
+
+            {
+                images.map((image: any)=> {
+                    return (
+                        <>
+                            <div className={style.ToolBar}>
+                            <div className={style.RedDot}></div>
+                            <div className={style.YellowDot}></div>
+                            <div className={style.GreenDot}></div>
+                            <div className={style.SearchBar}></div>
+                        </div>
+                            <img src={image} alt="" />
+                        </>
+                    )
+                })
+            }
         </div>
     );
 };
